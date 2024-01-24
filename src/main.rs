fbd9352fd1f6dee36fd41533a9637bb1eac6323e
General Zod <pro.boudjemline@gmail.com>
2024-01-25 00:21:53 +0100

    $

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,6 @@ use z1d_cellular_automata::{flag::Flag, state::State, wca_storage::WCAStorage};
 fn main() {
     const MAX: usize = 20;
     let sequence: Vec<State> = vec![State::ALIVE,State::DEAD, State::DEAD, State::DEAD, State::DEAD, State::DEAD, State::ALIVE, State::ALIVE, State::DEAD, State::DEAD, State::ALIVE, State::ALIVE, State::ALIVE, State::ALIVE, State::DEAD, State::ALIVE, State::ALIVE, State::DEAD, State::DEAD, State::DEAD, State::DEAD, State::DEAD];
-    //     State::ALIVE,
-    //     State::DEAD,
-    //     State::DEAD,
-    //     State::DEAD,
-    //     State::DEAD,
-    //     State::DEAD,
-    //     State::ALIVE,
-    //     State::ALIVE,
-    //     State::DEAD,
-    //     State::DEAD,
-    //     State::ALIVE,
-    //     State::ALIVE,
-    //     State::ALIVE,
-    //     State::ALIVE,
-    //     State::DEAD,
-    //     State::ALIVE,
-    //     State::ALIVE,
-    //     State::DEAD,
-    //     State::DEAD,
-    //     State::DEAD,
-    //     State::DEAD,
-    //     State::DEAD,
-    // ];
 
     let mut container: WCAStorage = WCAStorage::new(Some(sequence));
     container.push(State::DEAD);
